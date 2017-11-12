5941b517302f4146bb38205a8bec46cd24d2f2ba
Dina <dina@muzzo.com>
2017-11-12 20:30:34 +0100

    Create 'Add a gig' form

---
+++ Muzzo/Controllers/GigController.cs
@@ -0,0 +1,30 @@
+﻿using Muzzo.Models;
+using Muzzo.ViewModels;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Muzzo.Controllers
+{
+    public class GigController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public GigController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+
+        //Gets form for adding a gig
+        [HttpGet]
+        public ActionResult Create()
+        {
+            AddGigFormViewModel model = new AddGigFormViewModel
+            {
+                Genres = _dbContext.Genres.ToList()
+            };
+
+            
+            return View(model);
+        }
+    }
+}
---
+++ Muzzo/ViewModels/AddGigFormViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Muzzo.Models;
+using System.Collections.Generic;
+
+namespace Muzzo.ViewModels
+{
+    public class AddGigFormViewModel
+    {
+        public string Venue { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public byte Genre { get; set; }
+        public IEnumerable<Genre> Genres { get; set; }
+    }
+}
