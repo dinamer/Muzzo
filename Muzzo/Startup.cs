1a28c400b0a182651d28ed3b87198497cacbd39f
Dina <Dina@DINA-PC>
2017-11-12 17:31:28 +0100

    Add project files.

---
+++ Muzzo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Muzzo.Startup))]
+namespace Muzzo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
