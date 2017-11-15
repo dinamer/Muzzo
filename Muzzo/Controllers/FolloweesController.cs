13d4d7cd52f4ea85b9e25aa3de05120d408f301f
Dina <dina@muzzo.com>
2017-11-15 20:08:15 +0100

    Add artists I'm following functionality.

---
+++ Muzzo/Controllers/FolloweesController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNet.Identity;
+using Muzzo.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Muzzo.Controllers
+{
+    public class FolloweesController : Controller
+    {
+        private ApplicationDbContext _dbContext;
+
+        public FolloweesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        // GET: Folowees
+        [Authorize]
+        public ActionResult Index()
+        {
+            var followerId = User.Identity.GetUserId();
+
+            IEnumerable<ApplicationUser> followees = _dbContext.Followings
+                                                    .Where(f => f.FollowerId == followerId)
+                                                    .Select(f => f.Followee)
+                                                    .ToList();
+                           
+
+            return View(followees);
+        }
+    }
+}
